caacd66816819921e13fa1c0db09b89a60231527
christian otkjær <christian.otkjaer@gmail.com>
2016-03-04 11:43:02 +0100

    Initial Commit

---
+++ Compare/Compare.h
@@ -0,0 +1,19 @@
+//
+//  Compare.h
+//  Compare
+//
+//  Created by Christian Otkjær on 04/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Compare.
+FOUNDATION_EXPORT double CompareVersionNumber;
+
+//! Project version string for Compare.
+FOUNDATION_EXPORT const unsigned char CompareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Compare/PublicHeader.h>
+
+
